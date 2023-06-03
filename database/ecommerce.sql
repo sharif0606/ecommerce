fd5ee41adfd6348b656eda579d06a3bb7d94d0df
ikshakil <ikssakil2@gmail.com>
2023-06-03 10:41:36 +0600

    change

--- database/ecommerce.sql
+++ database/ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 03, 2023 at 06:34 AM
+-- Generation Time: Jun 03, 2023 at 06:40 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
