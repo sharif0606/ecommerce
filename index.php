b9b0aea6b011f80449acc00114976fb86077bc7b
ikshakil <ikssakil2@gmail.com>
2023-06-07 10:56:12 +0600

    add product to frontend

--- index.php
+++ index.php
@@ -390,283 +390,33 @@
          </h2>
       </div>
       <div class="row">
-         <div class="col-sm-6 col-md-4 col-lg-4">
-            <div class="box">
-               <div class="option_container">
-                  <div class="options">
-                     
-                     <a href="product_details.php" class="option2">
-                     Buy Now
-                     </a>
-                  </div>
-               </div>
-               <div class="img-box">
-                  <img src="assets/images/p1.png" alt="">
-               </div>
-               <div class="detail-box">
-                  <h5>
-                     Men's Shirt
-                  </h5>
-                  <h6>
-                     $75
-                  </h6>
-               </div>
-            </div>
-         </div>
+      <?php
+         $data=$mysqli->common_select_query("SELECT * FROM `product_tbl` WHERE deleted_at is null");
+         if(!$data['error']){
+            foreach($data['data'] as $d){
+      ?>
+      
          <div class="col-sm-6 col-md-4 col-lg-4">
             <div class="box">
                <div class="option_container">
                   <div class="options">
                      
                      </a>
-                     <a href="product_details.php" class="option2">
-                     Buy Now
+                     <a href="product_details.php?id=<?= $d->id ?>" class="option2">
+                        Buy Now
                      </a>
                   </div>
                </div>
                <div class="img-box">
-                  <img src="assets/images/p2.png" alt="">
+                  <img src="<?= $base_url?>upload/product/<?= $d->picture ?>" alt="">
                </div>
                <div class="detail-box">
-                  <h5>
-                     Men's Shirt
-                  </h5>
-                  <h6>
-                     $80
-                  </h6>
+                  <h5><?= $d->name ?></h5>
+                  <h6>BDT <?= $d->price ?></h6>
                </div>
             </div>
          </div>
-         <div class="col-sm-6 col-md-4 col-lg-4">
-            <div class="box">
-               <div class="option_container">
-                  <div class="options">
-                     
-                     <a href="product_details.php" class="option2">
-                     Buy Now
-                     </a>
-                  </div>
-               </div>
-               <div class="img-box">
-                  <img src="assets/images/p3.png" alt="">
-               </div>
-               <div class="detail-box">
-                  <h5>
-                     Women's Dress
-                  </h5>
-                  <h6>
-                     $68
-                  </h6>
-               </div>
-            </div>
-         </div>
-         <div class="col-sm-6 col-md-4 col-lg-4">
-            <div class="box">
-               <div class="option_container">
-                  <div class="options">
-                      
-                     <a href="product_details.php" class="option2">
-                     Buy Now
-                     </a>
-                  </div>
-               </div>
-               <div class="img-box">
-                  <img src="assets/images/p4.png" alt="">
-               </div>
-               <div class="detail-box">
-                  <h5>
-                     Women's Dress
-                  </h5>
-                  <h6>
-                     $70
-                  </h6>
-               </div>
-            </div>
-         </div>
-         <div class="col-sm-6 col-md-4 col-lg-4">
-            <div class="box">
-               <div class="option_container">
-                  <div class="options">
-                      
-                     <a href="product_details.php" class="option2">
-                     Buy Now
-                     </a>
-                  </div>
-               </div>
-               <div class="img-box">
-                  <img src="assets/images/p5.png" alt="">
-               </div>
-               <div class="detail-box">
-                  <h5>
-                     Women's Dress
-                  </h5>
-                  <h6>
-                     $75
-                  </h6>
-               </div>
-            </div>
-         </div>
-         <div class="col-sm-6 col-md-4 col-lg-4">
-            <div class="box">
-               <div class="option_container">
-                  <div class="options">
-                      
-                     <a href="product_details.php" class="option2">
-                     Buy Now
-                     </a>
-                  </div>
-               </div>
-               <div class="img-box">
-                  <img src="assets/images/p6.png" alt="">
-               </div>
-               <div class="detail-box">
-                  <h5>
-                     Women's Dress
-                  </h5>
-                  <h6>
-                     $58
-                  </h6>
-               </div>
-            </div>
-         </div>
-         <div class="col-sm-6 col-md-4 col-lg-4">
-            <div class="box">
-               <div class="option_container">
-                  <div class="options">
-                     
-                     <a href="product_details.php" class="option2">
-                     Buy Now
-                     </a>
-                  </div>
-               </div>
-               <div class="img-box">
-                  <img src="assets/images/p7.png" alt="">
-               </div>
-               <div class="detail-box">
-                  <h5>
-                     Women's Dress
-                  </h5>
-                  <h6>
-                     $80
-                  </h6>
-               </div>
-            </div>
-         </div>
-         <div class="col-sm-6 col-md-4 col-lg-4">
-            <div class="box">
-               <div class="option_container">
-                  <div class="options">
-                      
-                     <a href="product_details.php" class="option2">
-                     Buy Now
-                     </a>
-                  </div>
-               </div>
-               <div class="img-box">
-                  <img src="assets/images/p8.png" alt="">
-               </div>
-               <div class="detail-box">
-                  <h5>
-                     Men's Shirt
-                  </h5>
-                  <h6>
-                     $65
-                  </h6>
-               </div>
-            </div>
-         </div>
-         <div class="col-sm-6 col-md-4 col-lg-4">
-            <div class="box">
-               <div class="option_container">
-                  <div class="options">
-                      
-                     <a href="product_details.php" class="option2">
-                     Buy Now
-                     </a>
-                  </div>
-               </div>
-               <div class="img-box">
-                  <img src="assets/images/p9.png" alt="">
-               </div>
-               <div class="detail-box">
-                  <h5>
-                     Men's Shirt
-                  </h5>
-                  <h6>
-                     $65
-                  </h6>
-               </div>
-            </div>
-         </div>
-         <div class="col-sm-6 col-md-4 col-lg-4">
-            <div class="box">
-               <div class="option_container">
-                  <div class="options">
-                     
-                     <a href="product_details.php" class="option2">
-                     Buy Now
-                     </a>
-                  </div>
-               </div>
-               <div class="img-box">
-                  <img src="assets/images/p10.png" alt="">
-               </div>
-               <div class="detail-box">
-                  <h5>
-                     Men's Shirt
-                  </h5>
-                  <h6>
-                     $65
-                  </h6>
-               </div>
-            </div>
-         </div>
-         <div class="col-sm-6 col-md-4 col-lg-4">
-            <div class="box">
-               <div class="option_container">
-                  <div class="options">
-                     
-                     <a href="product_details.php" class="option2">
-                     Buy Now
-                     </a>
-                  </div>
-               </div>
-               <div class="img-box">
-                  <img src="assets/images/p11.png" alt="">
-               </div>
-               <div class="detail-box">
-                  <h5>
-                     Men's Shirt
-                  </h5>
-                  <h6>
-                     $65
-                  </h6>
-               </div>
-            </div>
-         </div>
-         <div class="col-sm-6 col-md-4 col-lg-4">
-            <div class="box">
-               <div class="option_container">
-                  <div class="options">
-                     
-                     <a href="product_details.php" class="option2">
-                     Buy Now
-                     </a>
-                  </div>
-               </div>
-               <div class="img-box">
-                  <img src="assets/images/p12.png" alt="">
-               </div>
-               <div class="detail-box">
-                  <h5>
-                     Women's Dress
-                  </h5>
-                  <h6>
-                     $65
-                  </h6>
-               </div>
-            </div>
-         </div>
+      <?php } } ?>
       </div>
       <div class="btn-box">
          <a href="product.php">
