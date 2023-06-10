fffa8f42f33b9f71d26099df152ad16702183876
mojahidhouel <mojahidhouel95@gmail.com>
2023-06-10 13:25:30 +0600

    contact

--- include/sidebar.php
+++ include/sidebar.php
@@ -43,12 +43,40 @@
                            <i class="fa fa-search" aria-hidden="true"></i>
                            </button>
                         </form>
+                        <div class="content">
+            <!-- Navbar Start -->
+            <nav class="navbar navbar-expand bg-light navbar-light sticky-top px-4 py-0">
+                <a href="index.html" class="navbar-brand d-flex d-lg-none me-4">
+                    <h2 class="text-primary mb-0"><i class="fa fa-hashtag"></i></h2>
+                </a>
+                <a href="#" class="sidebar-toggler flex-shrink-0">
+                    <i class="fa fa-bars"></i>
+                </a>
+               
+                <div class="navbar-nav align-items-center ms-auto">
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                            <span class="d-none d-lg-inline-flex"></span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
+                            <a href="#" class="dropdown-item">My Profile</a>
+                            <a href="#" class="dropdown-item">Settings</a>
+                            <a href="logout.php" class="dropdown-item">Log Out</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <!-- Navbar End -->
+
                      </ul>
                   </div>
                </nav>
             </div>
          </header>
          <!-- end header section -->
+
+
+         
 
          <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
