96ab0f6d54133be569b061b1b0469dc0dd96a832
mojahidjouel <mojahidjouel95@gmail.com>
2023-06-14 13:55:06 +0600

    change

--- include/sidebar.php
+++ include/sidebar.php
@@ -12,7 +12,7 @@
                            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
                         </li>
                        
-                        <li class="nav-item active">
+                        <li class="nav-item">
                            <a class="nav-link" href="product.php">Products</a>
                         </li>
                         <li class="nav-item">
@@ -32,34 +32,34 @@
                            </button>
                         </form>            
                         <form class="form-inline">
-                           <button class="btn  my-2 my-sm-0 nav_search-btn" type="button">
+                           <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
                            <i class="fa fa-search" aria-hidden="true"></i>
                            </button>
                         </form>
                         <div class="content">
-                           <!-- Navbar Start -->
-                           <nav class="navbar navbar-expand bg-light navbar-light sticky-top px-4 py-0">
-                              <a href="index.html" class="navbar-brand d-flex d-lg-none me-4">
-                                 <h2 class="text-primary mb-0"><i class="fa fa-hashtag"></i></h2>
-                              </a>
-                              <a href="#" class="sidebar-toggler flex-shrink-0">
-                                 <i class="fa fa-bars"></i>
-                              </a>
-                              
-                              <div class="navbar-nav align-items-center ms-auto">
-                                 <div class="nav-item dropdown">
-                                       <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                                          <span class="d-none d-lg-inline-flex"></span>
-                                       </a>
-                                       <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
-                                          <a href="my_profile.php" class="dropdown-item">My Profile</a>
-                                          <a href=" setting.php" class="dropdown-item">Settings</a>
-                                          <a href="logout.php" class="dropdown-item">Log Out</a>
-                                       </div>
-                                 </div>
-                              </div>
-                           </nav>
-                           <!-- Navbar End -->
+            <!-- Navbar Start -->
+            <nav class="navbar navbar-expand bg-light navbar-light sticky-top px-4 py-0">
+                <a href="index.html" class="navbar-brand d-flex d-lg-none me-4">
+                    <h2 class="text-primary mb-0"><i class="fa fa-hashtag"></i></h2>
+                </a>
+                <a href="#" class="sidebar-toggler flex-shrink-0">
+                    <i class="fa fa-bars"></i>
+                </a>
+               
+                <div class="navbar-nav align-items-center ms-auto">
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                            <span class="d-none d-lg-inline-flex"></span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
+                            <a href="my_profile.php" class="dropdown-item">My Profile</a>
+                            <a href=" setting.php" class="dropdown-item">Settings</a>
+                            <a href="logout.php" class="dropdown-item">Log Out</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <!-- Navbar End -->
 
                      </ul>
                   </div>
